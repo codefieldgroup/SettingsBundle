1949e52a5e340e8ecda44aa1246c4e298c07d0f8
Jose Carlos Ramos Carmenates <jramoscarmenates@gmail.com>
2015-07-14 15:59:24 -0400

    Add to listener CfSettings return: object, json, array

--- Listener/CfSettingsListener.php
+++ Listener/CfSettingsListener.php
@@ -13,34 +13,37 @@ class CfSettingsListener
     /**
      * @param $container
      */
-    public function __construct( $container )
+    public function __construct($container)
     {
         $this->container = $container;
     }
 
     /**
      * @param null $parameter
-     * @param int $json_decode
+     * @param int $json_decode 0 => Object, 1 => Json, 2 => array
      *
      * @return mixed|null
      */
-    public function getSettings( $parameter = null, $json_decode = 0 )
+    public function getSettings($parameter = null, $json_decode = 0)
     {
-        if ($parameter !== null && is_string( $parameter ) && $parameter !== '') {
-            $em     = $this->container->get( 'doctrine.orm.entity_manager' );
-            $entity = $em->getRepository( 'cfSettingsBundle:CfSettings' )->findOneByParameter( $parameter );
+        if ($parameter !== null && is_string($parameter) && $parameter !== '') {
+            $em = $this->container->get('doctrine.orm.entity_manager');
+            $entity = $em->getRepository('cfSettingsBundle:CfSettings')->findOneByParameter($parameter);
             $result = $entity !== null ? $entity->getValue() : null;
         } else {
-            $em       = $this->container->get( 'doctrine.orm.entity_manager' );
-            $entities = $em->getRepository( 'cfSettingsBundle:CfSettings' )->findAll();
-            /* @var $entity CfSettings */
-            foreach ($entities as $entity) {
-                $result [$entity->getParameter()] = $entity->getValue();
-            }
+            $em = $this->container->get('doctrine.orm.entity_manager');
+            $result = $em->getRepository('cfSettingsBundle:CfSettings')->findAll();
         }
 
-        if ($json_decode === 1) {
-            $result = json_decode( $result, true );
+        if ($json_decode == 1) {
+            $result = json_decode($result, true);
+        } elseif ($json_decode == 2) {
+            $tmp_result = null;
+            /* @var $entity CfSettings */
+            foreach ($result as $entity) {
+                $tmp_result [$entity->getParameter()] = $entity->getValue();
+            }
+            $result = $tmp_result;
         }
 
         return $result;
@@ -53,28 +56,30 @@ class CfSettingsListener
      *
      * @return mixed
      */
-    public function setSettings( $parameter, $value, $json_encode = 0 )
+    public function setSettings($parameter, $value, $json_encode = 0)
     {
-        $em     = $this->container->get( 'doctrine.orm.entity_manager' );
+        $em = $this->container->get('doctrine.orm.entity_manager');
         $entity = null;
-        if ($parameter !== null && is_string( $parameter ) && $parameter !== '') {
-            $entity = $em->getRepository( 'cfSettingsBundle:CfSettings' )->findOneByParameter( $parameter );
+        if ($parameter !== null && is_string($parameter) && $parameter !== '') {
+            $entity = $em->getRepository('cfSettingsBundle:CfSettings')->findOneByParameter($parameter);
         } else {
             return null;
         }
 
         if ($json_encode === 1) {
-            $value = json_encode( $value, JSON_FORCE_OBJECT );
+            $value = json_encode($value, JSON_FORCE_OBJECT);
         }
 
         if ($entity !== null) {
-            $entity->setValue( $value );
+            $entity->setValue($value);
         } else {
-            $params = [ 'parameter' => $parameter, 'value' => $value ];
-            $entity = $this->container->get( 'cf.commonbundle.miscellaneous' )->bindParameters( new CfSettings(), $params );
+            $params = ['parameter' => $parameter, 'value' => $value];
+            $entity = $this->container->get('cf.commonbundle.miscellaneous')->bindParameters(new CfSettings(), $params);
         }
 
-        $em->persist( $entity );
+        $em->persist($entity);
         $em->flush();
+
+        return $entity;
     }
 }
