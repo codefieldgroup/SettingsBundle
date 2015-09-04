d19ee0173658de59ba459942ad2581c478580b10
Jose Carlos Ramos Carmenates <jramoscarmenates@gmail.com>
2015-09-03 21:47:47 -0400

    History log disabled.

--- Entity/CfSettings.php
+++ Entity/CfSettings.php
@@ -124,13 +124,13 @@ class CfSettings
         return $this->description;
     }
 
-    /**
-     * Get Parameters History Log
-     *
-     * @return array
-     */
-    public function getParametersHistoryLog()
-    {
-        return ['parameter', 'value', 'description'];
-    }
+//    /**
+//     * Get Parameters History Log
+//     *
+//     * @return array
+//     */
+//    public function getParametersHistoryLog()
+//    {
+//        return ['parameter', 'value', 'description'];
+//    }
 }
